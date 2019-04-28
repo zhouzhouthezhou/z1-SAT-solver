f347b2fb170e6e57cd3138a3c06744cb7e5553d8
zhouzhouthezhou <kylezhou2222@gmail.com>
2019-04-28 13:46:09 -0600

    postfix notation now works, can use parenthesis. Currently z1 can only negate popositional variable and not statements

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ from z1 import *
 
 a = Bool('a')
 b = Bool('b')
-c = Bool('c')
 
 s = Solver()
 
 #s.add('(a&~b)|~c')
 #s.add(Or(And(a, Not(b)), Not(c)))
+s.add(Imply(a, And(Not(a), Not(b))))
 
 print(s.check())
 print(s.model())
